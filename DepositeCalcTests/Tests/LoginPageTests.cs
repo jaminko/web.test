85bff5940d7b5ecbca208f15d390849b6bf02f9d
iv.mocherniuk <ivan.mocherniuk@skelia.com>
2022-10-28 13:48:41 +0300

    Add Valid Login Test

--- DepositeCalcTests/Tests/LoginPageTests.cs
+++ DepositeCalcTests/Tests/LoginPageTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
 using System.Threading;
 
 namespace DepositeCalcTests.Tests
@@ -103,6 +104,30 @@ namespace DepositeCalcTests.Tests
 
             // Assert
             Assert.AreEqual("Password", passwordFldSignature.Text);
+            driver.Quit();
+        }
+
+        [Test]
+        public void ValidLoginTest()
+        {
+            // Arrange
+            ChromeOptions options = new ChromeOptions { AcceptInsecureCertificates = true };
+            IWebDriver driver = new ChromeDriver(options);
+
+            driver.Url = "https://localhost:5001/";
+            IWebElement loginFld = driver.FindElement(By.Id("login"));
+            IWebElement passworldFld = driver.FindElement(By.Id("password"));
+            IWebElement loginBtn = driver.FindElement(By.XPath("//button[@id='login']"));
+            String currentURL = driver.Url;
+
+            // Act
+            loginFld.SendKeys("test");
+            passworldFld.SendKeys("newyork1");
+            loginBtn.Click();
+
+            // Assert
+            Assert.IsTrue(driver.FindElement(By.Id("amount")).Displayed);
+            Assert.AreNotEqual(currentURL, driver.Url);
             driver.Quit();
         }
     }
