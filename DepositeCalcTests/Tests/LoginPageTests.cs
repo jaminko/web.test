26bfe91bf9dbaee704f1f6c2d38474526aa98d93
iv.mocherniuk <ivan.mocherniuk@skelia.com>
2022-10-28 13:16:47 +0300

    Change expected signature results according to requirements in README file

--- DepositeCalcTests/Tests/LoginPageTests.cs
+++ DepositeCalcTests/Tests/LoginPageTests.cs
@@ -87,7 +87,22 @@ namespace DepositeCalcTests.Tests
             IWebElement loginFldSignature = driver.FindElement(By.XPath("//th[@class='user']"));
 
             // Assert
-            Assert.AreEqual("User:", loginFldSignature.Text);
+            Assert.AreEqual("User", loginFldSignature.Text);
+            driver.Quit();
+        }
+
+        [Test]
+        public void PasswordFieldSignatureTest()
+        {
+            // Arrange
+            ChromeOptions options = new ChromeOptions { AcceptInsecureCertificates = true };
+            IWebDriver driver = new ChromeDriver(options);
+
+            driver.Url = "https://localhost:5001/";
+            IWebElement passwordFldSignature = driver.FindElement(By.XPath("//th[@class='pass']"));
+
+            // Assert
+            Assert.AreEqual("Password", passwordFldSignature.Text);
             driver.Quit();
         }
     }
