f5618a110ffcfb07d51ae72e5d803cac8cb362ff
Sergii Dmytrenko <dmytrenko.sergii@gmail.com>
2022-11-03 08:38:27 +0200

    #23 fix 32nd of July bug

--- WebSite/wwwroot/js/calculator.js
+++ WebSite/wwwroot/js/calculator.js
@@ -20,17 +20,13 @@ VerifyTerm = function (id, min) {
 }
 
 Date.prototype.yyyymmdd = function () {
-    // December 32
-    //var dd = this.getDate();
+    var dd = this.getDate();
     var mm = this.getMonth() + 1;
-    var dd = this.getDate() + 1;
     return [(dd > 9 ? '' : '0') + dd, (mm > 9 ? '' : '0') + mm, this.getFullYear()].join('/');
 };
 
 CalculateDate = function () {
-    // December 32
-    //var day = document.getElementById('day').selectedIndex + 1;
-    var day = document.getElementById('day').selectedIndex;
+    var day = document.getElementById('day').selectedIndex + 1;
     var month = document.getElementById('month').selectedIndex;
     var year = document.getElementById('year').value;
     var days = Number(document.getElementById('term').value);
@@ -89,7 +85,6 @@ async function ResetMonth() {
         default:
             AddOptions('day', 1, leapYear ? 29 : 28);
             //SetDay(day <= (leapYear ? 29 : 28) ? day : (leapYear ? 29 : 28));
-            //AddOptions('day', 1, 28);
             SetDay(day < 29 ? day : 28);
             break;
     }
