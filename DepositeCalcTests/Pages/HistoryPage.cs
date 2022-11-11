874a99b525b40f296556d3a5ace53d8c726b1f69
iv.mocherniuk <ivan.mocherniuk@skelia.com>
2022-11-11 22:22:13 +0200

    - Changed LastCalculationHistoryTest (using arrays) - Changed ColumnSignaturesTest (using arrays) - Code refactoring - Resolved ALL conversations

--- DepositeCalcTests/Pages/HistoryPage.cs
+++ DepositeCalcTests/Pages/HistoryPage.cs
@@ -13,18 +13,8 @@ namespace DepositeCalcTests.Pages
 
         private IWebElement CalculatorLnk => driver.FindElement(By.XPath("//div[text() = 'Calculator']"));
         private IWebElement ClearBtn => driver.FindElement(By.XPath("//button[text() = 'Clear']"));
-        private IWebElement HistoryTableRow1Coloumn1Field => driver.FindElement(By.XPath("//table/tr[1]/th[1]"));
-        private IWebElement HistoryTableRow1Coloumn2Field => driver.FindElement(By.XPath("//table/tr[1]/th[2]"));
-        private IWebElement HistoryTableRow1Coloumn3Field => driver.FindElement(By.XPath("//table/tr[1]/th[3]"));
-        private IWebElement HistoryTableRow1Coloumn4Field => driver.FindElement(By.XPath("//table/tr[1]/th[4]"));
-        private IWebElement HistoryTableRow1Coloumn7Field => driver.FindElement(By.XPath("//table/tr[1]/th[7]"));
-        private IWebElement HistoryTableRow1Coloumn8Field => driver.FindElement(By.XPath("//table/tr[1]/th[8]"));
+        private IWebElement HeadersRow => driver.FindElement(By.XPath("//tr[@class='data-th']"));
         private IList<IWebElement> HistoryTableRowsList => driver.FindElements(By.XPath("//tr[@class='data-td']"));
-        private IWebElement HistoryTableRow2Coloumn1Field => driver.FindElement(By.XPath("//table/tr[2]/td[1]"));
-        private IWebElement HistoryTableRow2Coloumn2Field => driver.FindElement(By.XPath("//table/tr[2]/td[2]"));
-        private IWebElement HistoryTableRow2Coloumn3Field => driver.FindElement(By.XPath("//table/tr[2]/td[3]"));
-        private IWebElement HistoryTableRow2Coloumn7Field => driver.FindElement(By.XPath("//table/tr[2]/td[7]"));
-        private IWebElement HistoryTableRow2Coloumn8Field => driver.FindElement(By.XPath("//table/tr[2]/td[8]"));
 
         public bool IsOpened()
         {
@@ -37,38 +27,15 @@ namespace DepositeCalcTests.Pages
             return new CalculatorPage(driver);
         }
 
-        public HistoryPage Clear()
+        public void Clear()
         {
             ClearBtn.Click();
             new WebDriverWait(driver, TimeSpan.FromMilliseconds(500)).Until(_ => ClearBtn.Enabled);
-            return new HistoryPage(driver);
         }
-
-        public string HistoryTableRow1Column1 => HistoryTableRow1Coloumn1Field.Text;
-
-        public string HistoryTableRow1Column2 => HistoryTableRow1Coloumn2Field.Text;
-
-        public string HistoryTableRow1Column3 => HistoryTableRow1Coloumn3Field.Text;
-
-        public string HistoryTableRow1Column4 => HistoryTableRow1Coloumn4Field.Text;
-
-        public string HistoryTableRow1Column7 => HistoryTableRow1Coloumn7Field.Text;
-
-        public string HistoryTableRow1Column8 => HistoryTableRow1Coloumn8Field.Text;
-
-        public string HistoryTableRow2Column1 => HistoryTableRow2Coloumn1Field.Text;
-
-        public string HistoryTableRow2Column2 => HistoryTableRow2Coloumn2Field.Text;
-
-        public string HistoryTableRow2Column3 => HistoryTableRow2Coloumn3Field.Text;
-
-        public string HistoryTableRow2Column7 => HistoryTableRow2Coloumn7Field.Text;
-
-        public string HistoryTableRow2Column8 => HistoryTableRow2Coloumn8Field.Text;
 
         public int HistoryTableNumberOfRows => HistoryTableRowsList.Count;
 
-        public List<string> FirstRowValues
+        public List<string> LastCalculationsInFirstRow
         {
             get
             {
@@ -82,9 +49,23 @@ namespace DepositeCalcTests.Pages
             }
         }
 
+        public List<string> HeadersForHistoryTable
+        {
+            get
+            {
+                var result = new List<string>();
+                var headerValues = HeadersRow.FindElements(By.XPath("./th"));
+                foreach (var cel in headerValues)
+                {
+                    result.Add(cel.Text);
+                }
+                return result;
+            }
+        }
+
         public HistoryPage WeitForReady()
         {
-            new WebDriverWait(driver, TimeSpan.FromMilliseconds(500)).Until(_ => FirstRowValues.Count != 0);
+            new WebDriverWait(driver, TimeSpan.FromSeconds(2)).Until(_ => LastCalculationsInFirstRow.Count != 0);
             return new HistoryPage(driver);
         }
     }
--- DepositeCalcTests/Tests/HistoryPageTests.cs
+++ DepositeCalcTests/Tests/HistoryPageTests.cs
@@ -27,39 +27,40 @@ namespace DepositeCalcTests.Tests
         [Test]
         public void TitleTest()
         {
-            // Arrange
-            string historyPageTitle = "History";
-
             // Assert
-            Assert.AreEqual(historyPageTitle, driver.Title, "Incorrect title");
+            Assert.AreEqual("History", driver.Title, "Incorrect title");
         }
 
-        [TestCase("177", "64", "25")]
-        public void LastCalculationHistoryTest(string depositAmount, string interestRate, string investmentTerm)
+        [Test]
+        public void LastCalculationHistoryTest()
         {
             // Arrange
             var calculatorPage = new CalculatorPage(driver);
-            var x = historyPage.FirstRowValues;
+            var depositAmount = "123";
+            var interestRate = "100";
+            var investmentTerm = "321";
+            var expectedLastCalculationValues = new[]
+            {
+                "123.00",
+                "100",
+                "321",
+                "365",
+                "11/11/2022",
+                "28/09/2023",
+                "231.17",
+                "108.17",
+            };
 
             // Act
             driver.Url = "https://localhost:5001/Calculator";
             calculatorPage.FinancialYear = "365";
             calculatorPage.FillingMandatoryTextFields(depositAmount, interestRate, investmentTerm);
             calculatorPage.Calculate();
-            string expectedInterestEarned = calculatorPage.InterestEarned;
-            string expectedIncome = calculatorPage.Income;
             calculatorPage.OpenHistory();
             historyPage.WeitForReady();
 
             // Assert
-            Assert.Multiple(() =>
-            {
-                Assert.AreEqual(depositAmount, historyPage.HistoryTableRow2Column1, "Incorect deposit amount value for last calculation in the history table");
-                Assert.AreEqual(interestRate, historyPage.HistoryTableRow2Column2, "Incorect rate of interest value for last calculation in the history table");
-                Assert.AreEqual(investmentTerm, historyPage.HistoryTableRow2Column3, "Incorect investment term value for last calculation in the history table");
-                Assert.AreEqual(expectedIncome, historyPage.HistoryTableRow2Column7, "Incorect Interest value for last calculation in the history table");
-                Assert.AreEqual(expectedInterestEarned, historyPage.HistoryTableRow2Column8, "Incorect Income value for last calculation in the history table");
-            });
+            Assert.AreEqual(expectedLastCalculationValues, historyPage.LastCalculationsInFirstRow);
         }
 
         [Test]
@@ -74,26 +75,30 @@ namespace DepositeCalcTests.Tests
         {
             // Act
             var unclearedHistoryPage = historyPage.WeitForReady();
-            var clearedHistoryPage = historyPage.Clear();
+            historyPage.Clear();
 
             // Assert
             Assert.AreEqual(0, historyPage.HistoryTableNumberOfRows, "Clear CTA button works incorrectly - the History table was not cleared");
         }
 
-        [TestCase("Deposit amount", "Rate of interest", "Investment term", "Financial year", "Income", "Intereset earned")]
-        public void ColumnSignaturesTest(string DepositAmountSignature, string RateOfInterestSignature, string InvestmentTermSignature,
-                                       string FinYearSignature, string IncomeSignature, string InterestEarnedSignature)
+        [Test]
+        public void ColumnSignaturesTest()
         {
-            // Assert
-            Assert.Multiple(() =>
+            // Arrange
+            var expectedHeaders = new[]
             {
-                Assert.AreEqual(DepositAmountSignature, historyPage.HistoryTableRow1Column1, "Incorect signature for deposit amount column in the history table");
-                Assert.AreEqual(RateOfInterestSignature, historyPage.HistoryTableRow1Column2, "Incorect signature for rate of interest column in the history table");
-                Assert.AreEqual(InvestmentTermSignature, historyPage.HistoryTableRow1Column3, "Incorect signature for investment term column in the history table");
-                Assert.AreEqual(FinYearSignature, historyPage.HistoryTableRow1Column4, "Incorect signature for financial year column in the history table");
-                Assert.AreEqual(IncomeSignature, historyPage.HistoryTableRow1Column7, "Incorect signature interest column in the history table");
-                Assert.AreEqual(InterestEarnedSignature, historyPage.HistoryTableRow1Column8, "Incorect signature for intereset earned column in the history table");
-            });
+                "Amount",
+                "%",
+                "Term",
+                "Year",
+                "From",
+                "To",
+                "Income",
+                "Interest",
+            };
+
+            // Assert
+            Assert.AreEqual(expectedHeaders, historyPage.HeadersForHistoryTable, "Incorect column signature velue in history table");
         }
     }
 }
