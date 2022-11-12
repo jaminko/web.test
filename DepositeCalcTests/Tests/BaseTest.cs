46ae13dfb9c6ce4d5ef433001aba5ea902d4ab6f
Sergii Dmytrenko <30838113+varajan@users.noreply.github.com>
2022-11-12 08:22:10 +0200

    Try to hide debug information messages

--- DepositeCalcTests/Tests/BaseTest.cs
+++ DepositeCalcTests/Tests/BaseTest.cs
@@ -11,14 +11,19 @@ namespace DepositeCalcTests.Tests
 
         public void InitDriver(string url)
         {
-            ChromeOptions options = new ChromeOptions
+            var chromeDriverService = ChromeDriverService.CreateDefaultService();
+            chromeDriverService.HideCommandPromptWindow = true;
+            chromeDriverService.SuppressInitialDiagnosticInformation = true;
+            
+            var options = new ChromeOptions
             {
                 UnhandledPromptBehavior = UnhandledPromptBehavior.Ignore,
                 AcceptInsecureCertificates = true
             };
             options.AddArgument("--silent");
             options.AddArgument("log-level=3");
-            driver = new ChromeDriver(options);
+            
+            driver = new ChromeDriver(chromeDriverService, options);
             driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             driver.Url = url;
