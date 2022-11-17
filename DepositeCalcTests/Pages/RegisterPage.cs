b123d4da2cae221a41021607ef2af1d8b958a88b
iv.mocherniuk <ivan.mocherniuk@skelia.com>
2022-11-17 17:33:22 +0200

    - Add RemindPasswordView class - Add RegisterPage class

---
+++ DepositeCalcTests/Pages/RegisterPage.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace DepositeCalcTests.Pages
+{
+    internal class RegisterPage : BasePage
+    {
+        public RegisterPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        private IWebElement LoginFld => driver.FindElement(By.XPath("//input[@id='login']"));
+        private IWebElement EmailFld => driver.FindElement(By.XPath("//th[text()='Email:']/..//input"));
+        private IWebElement PassworldFld => driver.FindElement(By.XPath("//th[text()='Password:']/..//input"));
+        private IWebElement ConfirmPasswordFld => driver.FindElement(By.XPath("//th[text()='Confirm password:']/..//input"));
+        private IWebElement ReturnToLoginLnk => driver.FindElement(By.XPath("//div[@onclick='Logout()']"));
+        private IWebElement RegisterBtn => driver.FindElement(By.XPath("//button[@id='register']"));
+
+        public LoginPage Login()
+        {
+            ReturnToLoginLnk.Click();
+            return new LoginPage(driver);
+        }
+
+        public string ErrorMessage
+        {
+            get
+            {
+                var locator = By.Id("errorMessage");
+                IWebElement mainErrMessage = driver.FindElement(locator);
+                new WebDriverWait(driver, TimeSpan.FromSeconds(2)).Until(_ => mainErrMessage.Text.Length > 0);
+                return mainErrMessage.Text;
+            }
+        }
+
+        public LoginPage Login(string login, string email, string password)
+        {
+            LoginFld.SendKeys(login);
+            EmailFld.SendKeys(email);
+            PassworldFld.SendKeys(password);
+            ConfirmPasswordFld.SendKeys(password);
+            RegisterBtn.Click();
+            return new LoginPage(driver);
+        }
+
+        public bool IsOpened()
+        {
+            bool isConfirmPasswordFldPresent = driver.FindElements(By.XPath("//th[text()='Confirm password:']/..//input")).Count > 0;
+            return isConfirmPasswordFldPresent;
+        }
+    }
+}
---
+++ DepositeCalcTests/Pages/RemindPasswordView.cs
@@ -0,0 +1,81 @@
+﻿using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System;
+using SeleniumExtras.WaitHelpers;
+using System.Linq;
+
+namespace DepositeCalcTests.Pages
+{
+    internal class RemindPasswordView
+    {
+        protected readonly IWebDriver driver;
+        public RemindPasswordView(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        private string _frameId = "remindPasswordView";
+        private IWebElement RemindPasswordForm => driver.FindElement(By.Id(_frameId));
+        private IWebElement CloseBtn => driver.FindElement(By.XPath("//button[text()='x']"));
+        public bool IsShown => RemindPasswordForm.Displayed;
+        private IWebElement EmailFld => driver.FindElement(By.XPath("//input[@placeholder='Email address']"));
+        private IWebElement SendBtn => driver.FindElement(By.XPath("//button[text()='Send']"));
+        private IWebElement RemindPasswordBtn => driver.FindElement(By.XPath("//button[@id='remindBtn']"));
+
+        private string ErrMessage
+        {
+            get
+            {
+                var locator = By.Id("message");
+                IWebElement errMessageForRemindPasswordForm = driver.FindElement(locator);
+                new WebDriverWait(driver, TimeSpan.FromSeconds(2)).Until(_ => errMessageForRemindPasswordForm.Text.Length > 0);
+                return errMessageForRemindPasswordForm.Text;
+            }
+        }
+
+        public void Open()
+        {
+            RemindPasswordBtn.Click();
+        }
+
+        public void Close()
+        {
+            driver.SwitchTo().Frame(_frameId);
+            CloseBtn.Click();
+            driver.SwitchTo().DefaultContent();
+        }
+
+        public (bool IsSuccessful, string Message) RemindPassword(string email)
+        {
+            driver.SwitchTo().Frame(_frameId);
+            EmailFld.SendKeys(email);
+            SendBtn.Click();
+            if (IsAlertPresent())
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromMilliseconds(300));
+                wait.Until(ExpectedConditions.AlertIsPresent());
+                IAlert alert = driver.SwitchTo().Alert();
+                var result = (true, alert.Text);
+                alert.Accept();
+                return result;
+            }
+            else
+            {
+                return (false, ErrMessage);
+            }
+        }
+
+        public bool IsAlertPresent()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException Ex)
+            {
+                return false;
+            }
+        }
+    }
+}
